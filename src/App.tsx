858076afc68dde8a89aec08e1d0fd807ce14e21d
Thomas Peacock <tom.peacock@cyberthy.com>
2023-05-20 13:19:03 +0100

    added access jwt tokens

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { faTrash, faPencil } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
+  const [token, setToken] = React.useState<any>(false);
+
   const [tasks, setTasks] = React.useState<any>(null);
   const [editing, setEditing] = React.useState<any>(null);
 
@@ -16,19 +18,130 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      await getTasks();
+      // check if user is authenticated 
+      await checkAuth();
+
+      if (token) {
+        await getTasks();
+      }
     })()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  async function checkAuth() {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      return;
+    } else {
+      try {
+        const res = await axios.get('/validate', {
+          headers: {
+            'x-access-token': token
+          }
+        });
+        setToken(token)
+      } catch (error) {
+        // console.log(error)
+      }
+    }
+  }
+
+
   async function getTasks() {
-    const res = await axios.get('/tasks');
-    setTasks(res.data);
+    try {
+      const res = await axios.get('/tasks', {
+        headers: {
+          'x-access-token': token
+        }
+      });
+      setTasks(res.data);
+    } catch (error) {
+      // console.log(error)
+    }
   }
 
   async function deleteTask(id: string) {
-    await axios.delete(`/tasks/${id}`);
-    await getTasks();
+    try {
+      await axios.delete(`/tasks/${id}`, {
+        headers: {
+          'x-access-token': token,
+        }
+      });
+      await getTasks();
+    } catch (error) {
+      // console.log(error)
+    }
+  }
+
+  if (!token) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Login</h1>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+              const errors: any = {};
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
+              }
+              return errors;
+            }}
+            onSubmit={async (values, { setSubmitting }) => {
+              try {
+                const res = await axios.post('/login', values);
+                console.log(res.data)
+                localStorage.setItem('token', res.data.token);
+                setToken(res.data.token);
+              } catch (error) {
+                // console.log(error)
+              }
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form className='task-form' onSubmit={handleSubmit}>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <p className='error-message'>
+                  {errors.email && touched.email && errors.email}
+                </p>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <p className='error-message'>
+                  {errors.password && touched.password && errors.password}
+                </p>
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </form>
+            )}
+          </Formik>
+        </header>
+      </div>
+    )
   }
 
   return (
@@ -47,7 +160,11 @@ function App() {
           onSubmit={async (values) => {
             // we are updating a task
             if (editing) {
-              const res = await axios.put(`/tasks/${editing._id}`, values);
+              const res = await axios.put(`/tasks/${editing._id}`, values, {
+                headers: {
+                  'x-access-token': token,
+                }
+              });
 
               const newTasks = tasks.map((task: any) => {
                 if (task._id === res.data._id) {
@@ -59,7 +176,11 @@ function App() {
               setTasks(newTasks);
             } else {
               // adding a new task
-              const res = await axios.post('/tasks', values);
+              const res = await axios.post('/tasks', values, {
+                headers: {
+                  'x-access-token': token,
+                }
+              });
               setTasks([...tasks, res.data]);
             }
 
