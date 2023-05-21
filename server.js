35404a8a7368beb6e9a60d1f8183cdacfe526114
Thomas Peacock <tom.peacock@cyberthy.com>
2023-05-21 16:11:06 +0100

    changed port

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.use(
 );
 
 // start express server on port 5000
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+app.listen(80, () => {
+  console.log("server started on port 80");
 });
